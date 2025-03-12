5f7b32b1ccc5b1b58db8e7cfb19be29d2ad27635
arbic <teo.maybebaby@gmail.com>
2025-03-12 11:42:53 +0500

    eskiz updated

--- src/eskiz/eskiz.service.ts
+++ src/eskiz/eskiz.service.ts
@@ -6,6 +6,7 @@ import { lastValueFrom } from 'rxjs'
 @Injectable()
 export class EskizService {
 	private token: string
+	private tokenExpiry: number = 0
 
 	constructor(
 		private readonly httpService: HttpService,
@@ -26,6 +27,8 @@ export class EskizService {
 				})
 			)
 			this.token = response.data.data.token
+			this.tokenExpiry = Date.now() + 86400 * 1000
+
 			console.log('TOKEN', this.token)
 		} catch (error) {
 			console.error('Ошибка авторизации в Eskiz:', error.message)
@@ -33,13 +36,54 @@ export class EskizService {
 		}
 	}
 
+	private async refreshToken() {
+		if (!this.token) {
+			await this.authenticate()
+			return
+		}
+
+		try {
+			const response = await lastValueFrom(
+				this.httpService.patch(
+					'https://notify.eskiz.uz/api/auth/refresh',
+					null,
+					{
+						headers: {
+							Authorization: `Bearer ${this.token}`
+						}
+					}
+				)
+			)
+
+			if ((response.data.message = 'token_generated')) {
+				this.token = response.data.data.token
+				this.tokenExpiry = Date.now() + 86400 * 1000
+			} else {
+				console.warn(
+					'Eskiz: Ошибка обновления токена, повторная авторизация...'
+				)
+				await this.authenticate()
+			}
+		} catch (error) {
+			console.error(
+				'Eskiz: Ошибка обновления токена, выполняем повторную аутентификацию:',
+				error.message
+			)
+			await this.authenticate()
+		}
+	}
+
+	private isTokenValid(): boolean {
+		return this.token && Date.now() < this.tokenExpiry
+	}
+
 	async sendSms(
 		phoneNumber: string,
 		message: string,
 		callbackUrl?: string
 	): Promise<void> {
-		if (!this.token) {
-			await this.authenticate()
+		if (!this.isTokenValid()) {
+			await this.refreshToken()
 		}
 		const normalizedPhone = phoneNumber.replace(/[^0-9]/g, '')
 
